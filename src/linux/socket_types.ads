0a335301f8206d296234b155c356120b037146a1
Daniel Norte de Moraes <danielcheagle@tutanota.com>
2024-02-20 09:09:42 -0300

    A Important Typo fix.  Thanks God and Mary and Cia!!  Enjoy!!

--- src/linux/socket_types.ads
+++ src/linux/socket_types.ads
@@ -31,8 +31,8 @@ is
       ai_socktype : Interfaces.C.int := 0;
       ai_protocol : Interfaces.C.int := 0;
       ai_addrlen  : socklen_t := 0;
-      ai_canonname  : Address :=  Null_Address;
       ai_addr       : Address := Null_Address;
+      ai_canonname  : Address :=  Null_Address;
       ai_next       : Address :=  Null_Address;
     end record
       with Convention => C;
